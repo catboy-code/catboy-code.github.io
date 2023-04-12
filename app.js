2a01a01420e5cb5f9d1dbeb570a9ce731b3f253c
Ash <newestideaz@gmail.com>
2023-04-12 18:59:35 +0530

    changes to the javascript to make the buttons work in a more modern ways

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+var darkButton = document.getElementById("darkButton");
+var expButton = document.getElementById("expButton");
+
 // dark mode and stuff
 
-function changeMode() {
+darkButton.addEventListener("click", () => {
     var app = document.getElementsByTagName("BODY")[0];
     if (localStorage.getItem("lightMode") == "dark") {
 	localStorage.setItem ("lightMode", "light");
@@ -9,4 +12,8 @@ function changeMode() {
 	localStorage.setItem ("lightMode", "dark");
 	app.setAttribute("light-mode", "dark");
     }		
-}
+})
+
+expButton.addEventListener("click", () => {
+	document.location='experiments.html'
+})
