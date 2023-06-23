816d9124a792b32fbcefd419902aa3bf22d14ff1
Ashutosh Kashyap <newestideaz@gmail.com>
2023-06-24 04:33:56 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // dark mode and stuff
 
 function changeMode() {
-    var app = document.getElementsByTagName("BODY")[0];
-    if (localStorage.getItem("lightMode") == "dark") {
-	localStorage.setItem ("lightMode", "light");
-	app.setAttribute("light-mode", "light");
-    } else {
-	localStorage.setItem ("lightMode", "dark");
-	app.setAttribute("light-mode", "dark");
-    }		
+  var app = document.getElementsByTagName("BODY")[0];
+  if (localStorage.getItem("lightMode") == "dark") {
+    localStorage.setItem("lightMode", "light");
+    app.setAttribute("light-mode", "light");
+  } else {
+    localStorage.setItem("lightMode", "dark");
+    app.setAttribute("light-mode", "dark");
+  }		
 }
